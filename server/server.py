40d296234ee7894db6f5eeb482ed5bc16d692c37
wgj <wes@futureprefect.com>
2023-11-06 15:45:17 -0700

    Replace unused 32k model with turbo beta

--- server/server.py
+++ server/server.py
@@ -23,7 +23,7 @@ from bsrp.server import (
 
 GPT3 = 'gpt-3.5-turbo'
 GPT4 = 'gpt-4'
-GPT4_32K = 'gpt-4-32k'
+GPT4_TURBO = 'gpt-4-1106-preview'
 
 
 @dataclass
@@ -54,8 +54,8 @@ class OpenAiModel:
 MODELS: Dict[str, OpenAiModel] = {
     GPT3: OpenAiModel(GPT3, "GPT3💲", 0.002 / 1000, 0.0015 / 1000, 1024 * 4 - 1, tiktoken.encoding_for_model(GPT3)),
     GPT4: OpenAiModel(GPT4, "GPT4💵", 0.06 / 1000, 0.03 / 1000, 1024 * 8 - 1, tiktoken.encoding_for_model(GPT4)),
-    GPT4_32K: OpenAiModel(GPT4_32K, "GPT4-32K💰", 0.012 / 1000, 0.06 /
-                          1000, 1024 * 32 - 1, tiktoken.encoding_for_model(GPT4_32K))
+    GPT4_TURBO: OpenAiModel(GPT4_TURBO, "GPT4 Turbo💵", 0.01 / 1000, 0.03 /
+                          1000, 128000 - 1, tiktoken.encoding_for_model(GPT4_TURBO))
 }
 
 DEFAULT_SYSTEM_MESSAGE = "You are a helpful and concise assistant."
